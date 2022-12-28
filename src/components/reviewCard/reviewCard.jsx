5d118dbd959f24c59b548f6d626a8ba75be6bd59
Alexander <sashad2003@gmail.com>
2022-12-28 09:38:23 +0200

    Add edit mode alpha version

--- src/components/reviewCard/reviewCard.jsx
+++ src/components/reviewCard/reviewCard.jsx
@@ -1,18 +1,22 @@
+import AppForm from "../form/form";
 import { IconEdit } from "../icons/editIcon";
 import { IconTrash } from "../icons/trashIcon";
 
 const ReviewCard = ({ image, name, review, onDelete, onEdit }) => {
     return (
         <div className="card review scale-up-center">
-            <img src={image} alt="" />
-            <div className="review-content">
-                <h2>{name}</h2>
-                <p>{review}</p>
+            <div className="review-wrapper">
+                <img src={image} alt="" />
+                <div className="review-content">
+                    <h2>{name}</h2>
+                    <p>{review}</p>
+                </div>
+                <div className="review-buttons">
+                    <button onClick={onDelete}><IconTrash /></button>
+                    <button onClick={onEdit}><IconEdit /></button>
+                </div>
             </div>
-            <div className="review-buttons">
-                <button onClick={onDelete}><IconTrash/></button>
-                <button onClick={onEdit}><IconEdit/></button>
-            </div>
+            <AppForm userName={name} content={review} />
         </div>
     )
 }
