2ff0a46e927e4bc105dd89696bf9319885afb6e0
Alexander <sashad2003@gmail.com>
2022-12-28 09:17:48 +0200

    Add LocalStorage hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,88 +2,84 @@ import { useState } from 'react';
 import AppForm from './components/form/form';
 import ReviewCard from './components/reviewCard/reviewCard';
 import imgGen from '@dudadev/random-img';
+import useLocalStorage from './hooks/useLocalStorage';
 
 function App() {
 
-    const [cardsData, setCardsData] = useState([]);
+    const [cardsData, setCardsData] = useLocalStorage('reviews', []);
     const [lastId, setLastId] = useState(0);
-
-
 
     const idGenerator = () => {
         setLastId(lastId + 1);
-        return lastId;
     }
-
 
     const onDeleteHandler = (id) => {
         setCardsData((current) => current.filter((item) => item.id !== id));
     }
 
 
-const addReview = (name, review) => {
+    const addReview = (name, review) => {
 
-    if (name && review) {
-        let nextId = idGenerator();
-        imgGen().then(avatarURL => {
-            const newItem = { id: nextId, name, review, image: avatarURL };
-            setCardsData([...cardsData, newItem]);
-        });
-        // const newItem = { id: nextId, name, review, image: `https://picsum.photos/100?random=${nextId}` };
-        // const newItem = { id: nextId, name, review, image: tempImage };
-        // setCardsData([...cardsData, newItem]);
-    } else {
-        alert('All fields are required')
+        if (name && review) {
+            let nextId = idGenerator();
+            imgGen().then(avatarURL => {
+                idGenerator();
+                const newItem = { id: lastId, name, review, image: avatarURL };
+                setCardsData([...cardsData, newItem]);
+            });
+        } else {
+            alert('All fields are required')
+        }
+
     }
 
-}
 
-const onSubmitHandler = (e) => {
-    e.preventDefault();
-    const inputName = document.querySelector("input");
-    const inputTextArea = document.querySelector("textarea");
-    addReview(inputName.value, inputTextArea.value);
-    clearForm();
-}
+    const reviewItems = cardsData.map(({ name, review, image, id }) => {
+        return (
+            <ReviewCard
+                key={id}
+                name={name}
+                review={review}
+                image={image}
+                onDelete={() => onDeleteHandler(id)}
+                onEdit={() => console.log("Edit")}
+            />
+        )
+    })
 
-const reviewItems = cardsData.map(({ name, review, image, id }) => {
+    const [userName, setUserName] = useState('');
+    const [content, setContent] = useState('');
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        // const inputName = document.querySelector("input");
+        // const inputTextArea = document.querySelector("textarea");
+        addReview(userName, content);
+        clearForm();
+    }
+
+    const handleChange = (e) => {
+        if (e.target.id === 'fName') {
+            setUserName(e.target.value);
+        } else {
+            setContent(e.target.value);
+        }
+    }
+
+    const clearForm = () => {
+        setUserName('');
+        setContent('');
+    }
+
+
     return (
-        <ReviewCard
-            key={id}
-            name={name}
-            review={review}
-            image={image}
-            onDelete={() => onDeleteHandler(id)}
-            onEdit={() => console.log("Edit")}
-        />
+        <div className="App">
+            <h1>User Reviews</h1>
+            {reviewItems}
+            <AppForm onSubmitHandler={onSubmitHandler} userName={userName} content={content} handleChange={handleChange} />
+
+        </div>
     )
-})
-
-const [userName, setUserName] = useState('');
-const [content, setContent] = useState('');
-
-const handleChange = (e) => {
-    if (e.target.id === 'fName') {
-        setUserName(e.target.value);
-    } else {
-        setContent(e.target.value);
-    }
-}
-
-const clearForm = () => {
-    setUserName('');
-    setContent('');
-}
-
-
-return (
-    <div className="App">
-        <h1>User Reviews</h1>
-        {reviewItems}
-        <AppForm onSubmitHandler={onSubmitHandler} userName={userName} content={content} handleChange={handleChange} />
-
-    </div>
-)
 }
 
 export default App
